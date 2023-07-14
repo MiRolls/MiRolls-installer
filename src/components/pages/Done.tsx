f093748214052ce64381a9fcc00a2fdf6d7e5101
liangmiQwQ <mi_yyds@126.com>
2023-07-14 22:26:45 +0800

    feat: go home after install finish

--- src/components/pages/Done.tsx
+++ src/components/pages/Done.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./global.css"
 import Steps from "../Steps";
+import {Navigate} from "react-router-dom";
+
+
 
 function Done() {
+    const [navigate, setNavigate] = useState(<></>)
+
+    const goHome = () => {
+        setNavigate(<Navigate to={"/"}></Navigate>)
+    }
+
     return (
         <>
             <div className={"page"}>
                 <Steps CompletedStep={3} step={3}></Steps>
                 <img src={"/favicon.png"} className={"logo"} alt={"MiRolls Install Program"}/>
                 <h1>You completed the install of MiRolls!</h1>
-                <h4>You must reboot MiRolls to access your site.</h4>
+                <button className={"nextStep"} onClick={goHome}></button>
+                <h4>Enjoy your own website!</h4>
+                <span>It's that easy to build questionnaire website</span>
+                {navigate}
             </div>
         </>
     );
