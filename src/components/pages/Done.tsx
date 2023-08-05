23b60821d1defb68e19a3171c75f4311c75e7278
liangmiQwQ <mi_yyds@126.com>
2023-08-05 11:36:14 +0800

    fix: button content and direct jump
    # type 字段包含:
    # feat：新功能（feature）
    # fix：修补bug
    # docs：文档（documentation）
    # style： 格式（不影响代码运行的变动）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试
    # chore：构建过程或辅助工具的变动

--- src/components/pages/Done.tsx
+++ src/components/pages/Done.tsx
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./global.css"
 import Steps from "../Steps";
-import {Navigate} from "react-router-dom";
-
 
 
 function Done() {
-    const [navigate, setNavigate] = useState(<></>)
 
     const goHome = () => {
-        setNavigate(<Navigate to={"/"}></Navigate>)
+        window.location.href = "/";
     }
 
     return (
@@ -18,10 +15,9 @@ function Done() {
                 <Steps CompletedStep={3} step={3}></Steps>
                 <img src={"/favicon.png"} className={"logo"} alt={"MiRolls Install Program"}/>
                 <h1>You completed the install of MiRolls!</h1>
-                <button className={"nextStep"} onClick={goHome}></button>
+                <button className={"nextStep"} onClick={goHome}>Access your own website</button>
                 <h4>Enjoy your own website!</h4>
                 <span>It's that easy to build questionnaire website</span>
-                {navigate}
             </div>
         </>
     );
